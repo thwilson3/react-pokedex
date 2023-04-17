f1c26fd25d68b00f12164a767e744ed00e8e4e79
mgregerson <mgregerson4321@gmail.com>
2023-04-17 18:58:45 -0400

    Ready for code review

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,4 +1,4 @@
-function Pokecard({card}) {
+function Pokecard({ card }) {
   return (
     <div className="Pokemon">
       <h3 className="Pokemon-name">{card.name} </h3>
@@ -7,10 +7,10 @@ function Pokecard({card}) {
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${card.id}.png`}
         alt="pokemon"
       />
-      <p className="Pokemon-type">type: {card.type}</p>
+      <p className="Pokemon-type">Type: {card.type}</p>
       <p className="Pokemon-exp">EXP: {card.base_experience}</p>
     </div>
   );
-};
+}
 
 export default Pokecard;
