63fa64614d314869ed7e1b1af80ab2945ec1b1a5
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-17 17:44:20 -0500

    got the modules functioning

---
+++ src/Pokecard.js
@@ -0,0 +1,16 @@
+function Pokecard({card}) {
+  return (
+    <div className="Pokemon">
+      <h3 className="Pokemon-name">{card.name} </h3>
+      <img
+        className="Pokemon-image"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${card.id}.png`}
+        alt="pokemon"
+      />
+      <p className="Pokemon-type">type: {card.type}</p>
+      <p className="Pokemon-exp">EXP: {card.base_experience}</p>
+    </div>
+  );
+};
+
+export default Pokecard;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,14 +1,7 @@
+import Pokecard from "./Pokecard";
+
 function Pokedex(props) {
-  return props.map((card) => (
-    <div className="Pokemon">
-      <h3 className="Pokemon-name">{card.name} </h3>
-      <img
-        className="Pokemon-image"
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${card.id}.png`}
-        alt="pokemon"
-      />
-      <p className="Pokemon-type">type: {card.type}</p>
-      <p className="Pokemon-exp">EXP: {card.base_experience}</p>
-    </div>
-  ));
+  return props.cards.map((card) => <Pokecard card={card} />);
 };
+
+export default Pokedex;
